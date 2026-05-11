c9907e9f369f50189a92de5b9a38e18633450332
aluno <aluno@lab01-09>
2026-05-11 09:32:25 -0300

    frhifgif

--- Almoxrifado/src/main/java/com/almoxarifado/Fornecedor.java
+++ Almoxrifado/src/main/java/com/almoxarifado/Fornecedor.java
@@ -9,5 +9,40 @@ package com.almoxarifado;
  * @author aluno
  */
 public class Fornecedor {
+    private int idFornecedor;
+    private String nome;
+    private int senha;
+    private boolean perfil;
     
+    public int getIdFornecedor(){
+        return idFornecedor;
+    }
+    
+    public void setIdFornecedor(int _idFornecedor){
+        idFornecedor = _idFornecedor;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome (String _nome){
+        nome = _nome;
+    }
+    
+    public String getSenha(){
+        return senha;
+    }
+    
+    public void setSenha(String _senha){
+       senha = _senha;
+    }
+    
+    public boolean getPerfil(){
+        return perfil;
+    }
+    
+    public void setPerfil(boolean _perfil){
+        perfil = _perfil;
+    }
 }
